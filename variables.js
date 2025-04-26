108fe391c8c25d378d37c9ab7a50be4ccb000d52
Vedant Karale <vedantkarale271@gmail.com>
2025-04-26 17:03:36 +0530

    local remote merge, variables

---
+++ variables.js
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+const constantVariable = 23;
+let letVariable = 25;
+var varVariable = 27; // Do not use, no block scope
+variable = 29;
+
+console.table([constantVariable, letVariable, varVariable, variable]);
