05a910689ead527e509dbb0fddde016bfbb36e2c
Vedant Karale <vedantkarale271@gmail.com>
2025-04-30 18:32:47 +0530

    javascript data types

---
+++ basics/datatypes.js
@@ -0,0 +1,5 @@
+// Datatypes -> Primitive | Non-Primitive
+
+// Primitive datatypes -> number, string, boolean, null, Undefined, Symbol, bigint
+
+// Non-primitive datatypes -> Object, Arrays, Function, Regular Expressions, Date Object
